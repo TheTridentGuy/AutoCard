5ab33895fb0fccb9cc6924ea8d9229ded137eee4
TheTridentGuy <99673331+TheTridentGuy@users.noreply.github.com>
2022-10-26 07:26:39 -0700

    Initial commit

---
+++ test.js
@@ -0,0 +1,19 @@
+JSON.stringify(
+    {
+        "msg": {
+            "font": "sans-serif",
+            "greet": "Happpy Birthday",
+            "confetti": {
+                "colors": ["red", "orange", "yellow", "green", "blue", "purple"],
+                "count": 500,
+                "after": 1000,
+                "repeat": 0
+            },
+            "reveal": {
+                "style": "flip",
+                "delay": 10,
+                "transform": 0.8
+            }
+        }
+    }
+)
